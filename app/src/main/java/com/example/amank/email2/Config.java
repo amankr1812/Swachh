09c4e606d238c71dbf852cd78e3f994413ebe509
Aman Kumar <aman.kumar1812.svs@gmail.com>
2019-07-03 15:39:04 +0530

    Update Config.java

--- app/src/main/java/com/example/amank/email2/Config.java
+++ app/src/main/java/com/example/amank/email2/Config.java
@@ -5,6 +5,6 @@ package com.example.amank.email2;
  */
 
 public class Config {
-    public static final String EMAIL ="swachh2017@gmail.com";
-    public static final String PASSWORD ="amanvivekvaibhavkavya";
+    public static final String EMAIL ="XXXXXXXXX@XXXXX.XXX"; //enter the email id
+    public static final String PASSWORD ="XXXXXXXXXXXXXXXXXX"; //enter the password
 }
